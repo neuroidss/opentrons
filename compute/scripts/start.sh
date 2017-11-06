17a6a0bdd66f43775750c9c6c26f6afc1dbf1c51
Jared Greene <jared@opentrons.com>
2017-11-06 13:13:20 -0500

    Add config file for DHCP and top-level compute dir
    Add the config file for the eth0 interface and the DHCP server that it
    provides. Also move the resin and pi specific work from `api` top-level
    directory to a new `compute` top-level directory

--- compute/scripts/start.sh
+++ compute/scripts/start.sh
@@ -6,5 +6,8 @@ echo "[BOOT] Starting server"
 cd /usr/src/api
 python /usr/src/api/opentrons/server/main.py '0.0.0.0':31950 &
 
+echo "[BOOT] Starting DHCP server"
+. /usr/src/compute/scripts/dhcp_server_init.sh
+
 echo "[BOOT] Advertising local service"
 python /usr/src/compute/scripts/announce_mdns.py
